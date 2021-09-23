b3a1d1655cf76148fc2a1e47c090479734ed4564
sagatun <eriksaga@stud.ntnu.no>
2021-09-23 13:27:47 +0200

    add backend

--- apps/food-for-you/src/app/app.tsx
+++ apps/food-for-you/src/app/app.tsx
@@ -2,10 +2,21 @@ import { useEffect, useState } from "react";
 import Header from "./components/Layout/Header";
 import axios from "axios";
 export function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await axios.get("http://localhost:3333/api");
+      setMessage(result.data.message);
+    };
+    fetch();
+  }, []);
+
   return (
     <>
       <Header />
       <p>hvorfor fungerer ikke dette?</p>
+      <h1>{message}</h1>
     </>
   );
 }
