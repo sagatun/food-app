f9007107e98a5d682ea0de23e47cbd737bf22b00
sagatun <eriksaga@stud.ntnu.no>
2021-09-23 11:53:17 +0200

    sjekke om cache fungerer

--- apps/food-for-you/src/app/app.tsx
+++ apps/food-for-you/src/app/app.tsx
@@ -5,14 +5,6 @@ export function App() {
   return (
     <>
       <Header />
-      <div>
-        <h1>Ny dag, nye muligheter</h1>
-        <h2>Kanskje teste ut server/klient i dag... </h2>
-        <h1>
-          <i>BLIR DETTE OPPDATERT? FUNGERER CACHEN?</i>
-          <p>Nå da?</p>
-        </h1>
-      </div>
     </>
   );
 }
